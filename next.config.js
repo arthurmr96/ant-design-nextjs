72ef411b52494e11a5f156ca225584da095527d4
arthurmr13@hotmail.com <arthurmr13@hotmail.com>
2022-09-21 10:59:43 -0300

    Finished customization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withLess = require("next-with-less");
+
+module.exports = withLess({
+    reactStrictMode: true,
+    swcMinify: true,
+    lessLoaderOptions: {
+        lessOptions: {
+            modifyVars: {
+                'primary-color': '#1DA57A',
+                'link-color': '#1DA57A',
+                'border-radius-base': '2px',
+            },
+            javascriptEnabled: true,
+        }
+    },
+});
