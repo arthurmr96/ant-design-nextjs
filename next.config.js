1dc2f704b042ad8b98a5d4872af933e7a9a89ae6
Arthur Ribeiro <arthurmr13@hotmail.com>
2022-09-22 11:46:43 -0300

    Working example

--- next.config.js
+++ next.config.js
@@ -4,15 +4,5 @@ const withLess = require("next-with-less");
 
 module.exports = withLess({
     reactStrictMode: true,
-    swcMinify: true,
-    lessLoaderOptions: {
-        lessOptions: {
-            modifyVars: {
-                'primary-color': '#1DA57A',
-                'link-color': '#1DA57A',
-                'border-radius-base': '2px',
-            },
-            javascriptEnabled: true,
-        }
-    },
+    swcMinify: true
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
-import type {NextPage} from 'next'
-import {Button, Layout} from 'antd'
+import type { NextPage } from 'next'
+import {Button, theme} from 'antd';
+import { Input } from 'antd';
+import {useEffect, useState} from "react";
 
-const {Header, Footer, Sider, Content} = Layout;
+const { Search } = Input;
 
 const Home: NextPage = () => {
     return (
-        <Layout>
-            <Header>Heade</Header>
-            <Content>
-                <Button type='primary'>Click me</Button>
-            </Content>
-            <Footer>Footer</Footer>
-        </Layout>
+        <div style={{margin: 200}}>
+            <div>
+                <Button type="primary">Primary Button</Button>
+                <Button type="dashed">Dashed Button</Button>
+                <br />
+                <Button type="text">Text Button</Button>
+                <Button type="link">Link Button</Button>
+                <br />
+                <br />
+                <br />
+            </div>
+            <div style={{width: 300}}>
+                <Search placeholder="input search loading default" loading />
+            </div>
+        </div>
     )
 }
 
